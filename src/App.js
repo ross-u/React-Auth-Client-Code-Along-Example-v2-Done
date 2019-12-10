c77ab64ea69c8a9da1db4ecf4c664256ced19858
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-12-10 15:46:30 +0100

    Update files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Private from './pages/Private';
+import Navbar from './components/Navbar';
+
+import AnonRoute from './components/AnonRoute';
+import PrivateRoute from './components/PrivateRoute';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <h1>Basic React Authentication</h1>
+
+        <Switch>
+          <AnonRoute exact path="/signup" component={Signup} />
+          <AnonRoute exact path="/login" component={Login} />
+          <PrivateRoute exact path="/private" component={Private} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
